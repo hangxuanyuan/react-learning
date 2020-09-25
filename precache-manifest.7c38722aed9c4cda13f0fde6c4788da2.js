1586efb9c02abff8bf3c165994f06610e746d289
hangxuanyuan <yuan_0817@live.com>
2020-09-25 22:34:22 +0800

    Updates

---
+++ precache-manifest.7c38722aed9c4cda13f0fde6c4788da2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c06592d0db7a31fb905db4c0cce600",
+    "url": "/react-learning/index.html"
+  },
+  {
+    "revision": "19f55a767ec088b24ce0",
+    "url": "/react-learning/static/css/main.e7a76dd3.chunk.css"
+  },
+  {
+    "revision": "cae303a678086002c6b6",
+    "url": "/react-learning/static/js/2.4109f84e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-learning/static/js/2.4109f84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19f55a767ec088b24ce0",
+    "url": "/react-learning/static/js/main.7ff5f131.chunk.js"
+  },
+  {
+    "revision": "6bdfb70c787afc3d2f0b",
+    "url": "/react-learning/static/js/runtime-main.4d8a87d7.js"
+  },
+  {
+    "revision": "e42afa52bf567dc162c4fa62f941b8a1",
+    "url": "/react-learning/static/media/bg.e42afa52.jpg"
+  }
+]);
